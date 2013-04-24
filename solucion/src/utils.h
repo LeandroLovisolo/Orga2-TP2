89d53a14a48017c9039feccadc78069957dc00c7
Leandro Lovisolo <leandro@leandro.me>
2013-04-24 08:41:37 -0300

    Conversión de tabs a espacios

--- solucion/src/utils.h
+++ solucion/src/utils.h
@@ -2,19 +2,19 @@
 #define __UTILS__H__
 
 void copiar_bordes (
-	unsigned char *src,
-	unsigned char *dst,
-	int m,
-	int n,
-	int row_size
+    unsigned char *src,
+    unsigned char *dst,
+    int m,
+    int n,
+    int row_size
 );
 
 void voltear_horizontal (
-	unsigned char *src,
-	unsigned char *dst,
-	int m,
-	int n,
-	int row_size
+    unsigned char *src,
+    unsigned char *dst,
+    int m,
+    int n,
+    int row_size
 );
 
 #endif /* !__UTILS__H__ */
