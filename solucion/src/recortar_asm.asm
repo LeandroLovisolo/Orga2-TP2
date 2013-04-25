ac88f112f1879e432b19696aa01eac7708fef77a
Leandro Lovisolo <leandro@leandro.me>
2013-04-25 15:27:41 -0300

    Resolución parcial

--- solucion/src/recortar_asm.asm
+++ solucion/src/recortar_asm.asm
@@ -53,20 +53,33 @@ ciclo_y:
 
 ciclo_x:
     mov rax, [rbp + 16]         ; rax = tam
-    and rax, 0x00000000FFFFFFFF ; Limpio la parte alta de rax
     sub rax, r10                ; rax = tam - x
     cmp eax, 16
     jl menos_de_16_columnas
 
-    ; Guardo en eax el desplazamiento en src
-    mov rax, r11    ; eax = y
-    mov rbx, r8     ; ebx = src_row_size
-    mul ebx         ; eax = y * src_row_size
-    add rax, r10    ; eax = y * src_row_size + x
-    and rax, 0x00000000FFFFFFFF ; Limpio la parte alta de rax
+    ;;;;;;;;;;;;;;;
+    ;; Esquina A ;;
+    ;;;;;;;;;;;;;;;
 
-    movdqu xmm0, [r12 + rax]
-    movdqu [r13 + rax], xmm0
+    ; Copio
+    mov rax, r11                ; eax = y
+    mov rbx, r8                 ; ebx = src_row_size
+    mul ebx                     ; eax = src_row_size * y
+    add rax, r10                ; eax = src_row_size * y + x
+    shl rax, 32                 ; Limpio parte alta de rax
+    shr rax, 32
+    movdqu xmm0, [r12 + rax]    ; xmm0 = [src + (src_row_size * y + x)]
+
+    ; Pego
+    mov rax, [rbp + 16]         ; eax = tam
+    add rax, r11                ; eax = tam + y
+    mov rbx, r9                 ; ebx = dst_row_size
+    mul ebx                     ; eax = dst_row_size * (tam + y)
+    add rax, [rbp + 16]         ; eax = dst_row_size * (tam + y) + tam
+    add rax, r10                ; eax = dst_row_size * (tam + y) + tam + x
+    shl rax, 32                 ; Limpio parte alta de rax
+    shr rax, 32
+    movdqu [r13 + rax], xmm0    ; [dst + (dst_row_size * (tam + y) + tam + x)] = xmm0
 
     push r8
     push r9
