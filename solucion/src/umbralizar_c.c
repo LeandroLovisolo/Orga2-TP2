fb37b7792a697968903d05448bc66dc868cff5fb
Lautaro Petaccio <lausuper@gmail.com>
2013-04-25 11:34:37 -0300

    Umbralizar ahora usa casteo a matriz

--- solucion/src/umbralizar_c.c
+++ solucion/src/umbralizar_c.c
@@ -11,19 +11,19 @@ void umbralizar_c (
 ) {
 	unsigned char (*src_matrix)[row_size] = (unsigned char (*)[row_size]) src;
 	unsigned char (*dst_matrix)[row_size] = (unsigned char (*)[row_size]) dst;
-	for(int y=0;y<row_size;y++) {
-		for(int x=0;x<row_size;x++) {
+	for(int y=0;y<m;y++) {
+		for(int x=0;x<n;x++) {
 			int posicion = (y * row_size) + x;
-			if((*src_matrix)[posicion] < min) { //Es valor de la escala de grises menor que el mínimo?
-				(*dst_matrix)[posicion] = 0;
+			if(src_matrix[y][x] < min) { //Es valor de la escala de grises menor que el mínimo?
+				dst_matrix[y][x] = 0;
 			}
-			else if((*src_matrix)[posicion] > max) { //Es valor de la escala de grises mayor que el máximo?
-				(*dst_matrix)[posicion] = 255;
+			else if(src_matrix[y][x] > max) { //Es valor de la escala de grises mayor que el máximo?
+				dst_matrix[y][x] = 255;
 			}
 			else {
-				 float ecuacion = ((*src_matrix)[posicion] / q);
+				 float ecuacion = (src_matrix[y][x] / q);
 				 unsigned char nuevoValor = floor(ecuacion) * q; //Realizo la división como punto flotante y uso la función float para la parte entera
-				(*dst_matrix)[posicion] = nuevoValor;
+				dst_matrix[y][x] = nuevoValor;
 			}
 		}
 		
