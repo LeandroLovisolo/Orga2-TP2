b10e67515459e4c3d622e0b7dade6c9e344b8c01
Lautaro Petaccio <lausuper@gmail.com>
2013-04-27 16:20:34 -0300

    Cambiado rotar según nueva imagen en enunciado, aparentemente tiene una pequeña diferencia con la original

--- solucion/src/rotar_c.c
+++ solucion/src/rotar_c.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <math.h>
 void rotar_c (
     unsigned char *src,
     unsigned char *dst,
@@ -9,24 +9,22 @@ void rotar_c (
 ) {
     unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-    double eqCx = n/2; //Longitud / 2
-    double eqCy = m/2; //Altura / 2
+    double eqCx = n/2.0; //Longitud / 2
+    double eqCy = m/2.0; //Altura / 2
     int cx = floor(eqCx); //Obtendo su parte entera
     int cy = floor(eqCy); //Obtengo su parte entera
     //Recorro la imágen de destino y le voy colocando el píxel correspondiente
-    for(int y=0;y<dst_row_size;y++) {
-    	for(int x=0;x<dst_row_size;x++) {
+    for(int y=0;y<m;y++) {
+    	for(int x=0;x<n;x++) {
     		double u = cx + ((sqrt(2)/2) * (x - cx)) - ((sqrt(2)/2) * (y - cy)); //Ecuación u
     		double v = cy + ((sqrt(2)/2) * (x - cx)) +  ((sqrt(2)/2) * (y - cy)); //Ecuación v
   			unsigned int newU = floor(u);
   			unsigned int newV = floor(v);
-  			unsigned int newPos = (newV  * src_row_size) + newU;
-    		int posicion = (y * dst_row_size) + x;
     		if((0 <= u) && (u <= n) && (0 <= v) && (v <= m)) { //Es lo mismo usando newU y newV
-    			(*dst_matrix)[posicion] = (*src_matrix)[newPos];
+    			dst_matrix[y][x] = src_matrix[newU][newV];
     		}
     		else {
-    			(*dst_matrix)[posicion] = 0;
+    			dst_matrix[y][x] = 0;
     		}
 
     	}
