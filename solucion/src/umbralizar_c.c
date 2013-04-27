d3438780b06cc0b0fd81d32b80db1000f67296d1
Lautaro Petaccio <lausuper@gmail.com>
2013-04-27 16:12:50 -0300

    Arreglados errores en umbralizar

--- solucion/src/umbralizar_c.c
+++ solucion/src/umbralizar_c.c
@@ -2,8 +2,8 @@
 void umbralizar_c (
 	unsigned char *src,
 	unsigned char *dst,
-	int m,
-	int n,
+	int m, //Hight
+	int n, //Width
 	int row_size,
 	unsigned char min,
 	unsigned char max,
@@ -11,9 +11,9 @@ void umbralizar_c (
 ) {
 	unsigned char (*src_matrix)[row_size] = (unsigned char (*)[row_size]) src;
 	unsigned char (*dst_matrix)[row_size] = (unsigned char (*)[row_size]) dst;
+	float ecuacion = 0.0;
 	for(int y=0;y<m;y++) {
 		for(int x=0;x<n;x++) {
-			int posicion = (y * row_size) + x;
 			if(src_matrix[y][x] < min) { //Es valor de la escala de grises menor que el mínimo?
 				dst_matrix[y][x] = 0;
 			}
@@ -21,9 +21,8 @@ void umbralizar_c (
 				dst_matrix[y][x] = 255;
 			}
 			else {
-				 float ecuacion = (src_matrix[y][x] / q);
-				 unsigned char nuevoValor = floor(ecuacion) * q; //Realizo la división como punto flotante y uso la función float para la parte entera
-				dst_matrix[y][x] = nuevoValor;
+				ecuacion = (src_matrix[y][x] / q);
+				dst_matrix[y][x] = floor(ecuacion) * q; //Asigno el nuevo valor
 			}
 		}
 		
