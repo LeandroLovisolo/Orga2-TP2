d3977e31a15f2b007dbdee9f92cbe2a45e3a1f1b
Lautaro Petaccio <lausuper@gmail.com>
2013-05-02 11:13:46 -0300

    Completadas implementaciones en c y asm de rotar

--- solucion/src/rotar_c.c
+++ solucion/src/rotar_c.c
@@ -9,13 +9,14 @@ void rotar_c (
 ) {
     unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-    int count = 0;
+    float cx = floor(n/2.0);
+    float cy = floor(m/2.0);
+    float u = 0.0; //Declaro afuera para ahorrar algunos ciclos
+    float v = 0.0; //Declaro afuera para ahorrar algunos ciclos
     for(int y = 0; y < m; y++) {
         for(int x = 0; x < n; x++) {
-            float cx = floor(n/2.0);
-            float cy = floor(m/2.0);
-            float u = cx + sqrt(2)/2.0 * (x - cx) - sqrt(2)/2.0 * (y - cy);
-            float v = cy + sqrt(2)/2.0 * (x - cx) + sqrt(2)/2.0 * (y - cy);
+            u = cx + sqrt(2)/2.0 * (x - cx) - sqrt(2)/2.0 * (y - cy);
+            v = cy + sqrt(2)/2.0 * (x - cx) + sqrt(2)/2.0 * (y - cy);
             if(0 <= (int) u && u < n && 0 <= (int) v && v < m) {
                 dst_matrix[(int) y][(int) x] = src_matrix[(int) v][(int) u];
             } else {
